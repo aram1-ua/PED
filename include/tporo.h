b2fde560c65ed5a6afb51eae2af465af5738f527
anja <aram1@alu.ua.es>
2025-06-04 18:14:02 +0200

    errores principios

--- include/tporo.h
+++ include/tporo.h
@@ -25,39 +25,39 @@ public:
     // Constructor a partir de una posición y un volumen
     TPoro(int, int, double);
     // Constructor a partir de una posición, un volumen y un color
-    TPoro(int, int, double, const char *);
+    TPoro(int, int, double, char *);
     // Constructor de copia
-    TPoro(const TPoro &);  // CAMBIO: const
+    TPoro(TPoro &);
     // Destructor
     ~TPoro();
     // Sobrecarga del operador asignación
-    TPoro & operator=(const TPoro &);  // CAMBIO: const
+    TPoro & operator=(TPoro &);
 
     // MÉTODOS
     // Sobrecarga del operador igualdad
-    bool operator==(const TPoro &) const;  // CAMBIO: const
+    bool operator==(TPoro &);
     // Sobrecarga del operador desigualdad
-    bool operator!=(const TPoro &) const;  // CAMBIO: const
+    bool operator!=(TPoro &);
     // Modifica la posición
     void Posicion(int, int);
     // Modifica el volumen
     void Volumen(double);
     // Modifica el color
-    void Color(const char *);
+    void Color(char *);
     // Devuelve la coordenada x de la posición
-    int PosicionX() const;  // CAMBIO: const
+    int PosicionX();
     // Devuelve la coordenada y de la posición
-    int PosicionY() const;  // CAMBIO: const
+    int PosicionY();
     // Devuelve el volumen
-    double Volumen() const;  // CAMBIO: const
+    double Volumen();
     // Devuelve el color
-    const char * Color() const;
+    char * Color();
     // Devuelve cierto si el poro está vacío
-    bool EsVacio() const;  // CAMBIO: const
+    bool EsVacio();
 
     // FUNCIONES AMIGAS
     // Sobrecarga del operador SALIDA
-    friend ostream & operator<<(ostream &, const TPoro &);  // CAMBIO: const
+    friend ostream & operator<<(ostream &, TPoro &);
 };
 
 #endif
